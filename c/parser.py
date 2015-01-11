0241ca2e61a7e50e118d8141c10f85bf204b2e74
Joel B. Mohler <joel@kiwistrawberry.us>
2015-01-11 14:46:15 -0500

    make parser deal with stdin

--- c/parser.py
+++ c/parser.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+import sys
+
 def pair(line):                                                                      
     return int(line[:16], 16), int(line[18:])
 
@@ -16,3 +19,7 @@ def candidates(exponent):
 
 def candidate_count(exponent):
     return sum(1 for _ in (n for n, divides in collatz_n() if divides>exponent and n<2**exponent))
+
+if __name__ == '__main__':
+    for line in sys.stdin.readlines():
+        print pair(line)
