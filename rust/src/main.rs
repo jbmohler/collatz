a36d574fed65a55e2091e54e53fc0873612a5a73
Joel B. Mohler <joel@kiwistrawberry.us>
2018-12-13 20:33:35 -0500

    working from a file rather than a in-memory vector

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,11 +1,31 @@
 extern crate itertools;
-
-use itertools::Itertools;
+use std::str;
+use std::error::Error;
+use std::io::SeekFrom;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
 
 struct BitSuffix {
     bitcount: i32,
     exemplar: i64,
     //sigstr: Vec<u8>,
+}
+
+impl BitSuffix {
+    fn into_bytes(&self) -> [u8; 26] {
+        let s = format!("{:12x} {:12}\n", self.exemplar, self.bitcount);
+        let mut readbuf = [0u8; 26];
+        readbuf.copy_from_slice(s.as_bytes());
+        return readbuf;
+    }
+
+    fn from_bytes(buf: &[u8; 26]) -> BitSuffix {
+        let s = str::from_utf8(buf).unwrap();
+        let i = i64::from_str_radix(s[0..12].trim_start(), 16).unwrap();
+        let bitcount = i32::from_str_radix(s[13..25].trim_start(), 10).unwrap();
+        return BitSuffix { bitcount: bitcount, exemplar: i };
+    }
 }
 
 //fn run_collatz(ex: i64) -> (i32, Vec<u8>) {
@@ -35,37 +55,62 @@ fn run_collatz(ex: i64) -> i32 {
 }
 
 fn main() {
-    let cutoff = 32;
+    let cutoff = 37;
     let mut bitcount = 1;
-    let mut sigs : Vec<BitSuffix> = Vec::new();
+    //let mut sigs : Vec<BitSuffix> = Vec::new();
+    let path = Path::new("collatz_exemplars.txt");
+    let display = path.display();
 
-    fn compute_and_push(ex : i64, newsigs : &mut Vec<BitSuffix>) {
+    let mut outfile = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}",
+                           display,
+                           why.description()),
+        Ok(file) => file,
+    };
+
+    let mut infile = match File::open(&path) {
+        Err(why) => panic!("couldn't open for reading {}: {}",
+                           display,
+                           why.description()),
+        Ok(file) => file,
+    };
+
+    fn compute_and_push(ex : i64, ofile : &mut File) {
         //let results = run_collatz(ex);
         //let xx = BitSuffix {bitcount: results.0, exemplar: ex, sigstr: results.1};
         let bitcount = run_collatz(ex);
         let xx = BitSuffix {bitcount: bitcount, exemplar: ex};
+        ofile.write_all(&xx.into_bytes());
         //println!("{:0}: {:1} -- {:?}", xx.exemplar, xx.bitcount, xx.sigstr);
-        newsigs.push(xx);
+        //newsigs.push(xx);
     };
 
     loop {
         println!("Check new bit patterns of lenth {}", bitcount);
 
-        let mut newsigs : Vec<BitSuffix> = Vec::new();
+        //let mut newsigs : Vec<BitSuffix> = Vec::new();
         let base = 1 << (bitcount - 1);
 
         if bitcount == 1 {
             // seed this
-            compute_and_push(1, &mut newsigs);
+            compute_and_push(1, &mut outfile);
         } else {
-            for sig in sigs.iter() {
+            infile.seek(SeekFrom::Start(0));
+            let mut readbuf = [0u8; 26];
+            loop {
+                infile.read_exact(&mut readbuf);
+
+                let sig = BitSuffix::from_bytes(&readbuf);
+                if sig.exemplar > base {
+                    break;
+                }
                 if sig.bitcount >= bitcount {
-                    compute_and_push(base+sig.exemplar, &mut newsigs);
+                    compute_and_push(base+sig.exemplar, &mut outfile);
                 }
             }
         }
 
-        sigs.extend(newsigs);
+        //sigs.extend(newsigs);
 
         bitcount += 1;
 
@@ -74,7 +119,10 @@ fn main() {
         }
     }
 
+    drop(infile);
+    drop(outfile);
 
+/*
     sigs.sort_by_key(|elt| elt.bitcount);
 
     for (k, grp) in &sigs.into_iter().group_by(|elt| elt.bitcount) {
@@ -82,5 +130,5 @@ fn main() {
             break;
         }
         println!("bit {}: count {}", k, grp.into_iter().count());
-    }
+    } */
 }
